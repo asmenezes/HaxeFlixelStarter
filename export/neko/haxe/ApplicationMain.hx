f07a7bba3445507cb9e545eda154d6b6104a598a
asmenezes <asmenezes23@gmail.com>
2021-01-23 09:31:02 -0500

    New Build

--- export/neko/haxe/ApplicationMain.hx
+++ export/neko/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "17";
+		app.meta["build"] = "20";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "ShootyPlat";
 		app.meta["name"] = "ShootyPlat";
