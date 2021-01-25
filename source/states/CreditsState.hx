05ebf194fe2e204b3d1a798a9200315169a5053c
asmenezes <asmenezes23@gmail.com>
2021-01-25 10:00:19 -0500

    Created the Credits

--- source/states/CreditsState.hx
+++ source/states/CreditsState.hx
@@ -4,6 +4,8 @@ import flixel.FlxState;
 import flixel.FlxG;
 import flixel.ui.FlxButton;
 import flixel.util.FlxColor;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
 class CreditsState extends FlxState{
    var playButton:FlxButton;
     override public function create():Void{
@@ -12,6 +14,13 @@ class CreditsState extends FlxState{
       playButton.y += 60;
       add(playButton);
       FlxG.camera.bgColor = FlxColor.fromString("#96ace8");
+      var disp = new FlxText();
+disp.text = "Enemy sprite by me. \nAll other art by Kenney. (Kenney.nl)\nSound Effects made in BFXR. (bfxr.net)\nMusic by Joth. (https://opengameart.org/users/joth)";
+disp.color = FlxColor.BLACK;
+disp.size = 15;
+disp.screenCenter();
+disp.y -= 100;
+add(disp);
         super.create();
     }
 
