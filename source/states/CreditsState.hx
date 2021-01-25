ec254b33f3dc210721f2ac74a191b81188d312a2
asmenezes <asmenezes23@gmail.com>
2021-01-25 10:04:49 -0500

    Formatted Credits

--- source/states/CreditsState.hx
+++ source/states/CreditsState.hx
@@ -15,13 +15,13 @@ class CreditsState extends FlxState{
       add(playButton);
       FlxG.camera.bgColor = FlxColor.fromString("#96ace8");
       var disp = new FlxText();
-disp.text = "Enemy sprite by me. \nAll other art by Kenney. (Kenney.nl)\nSound Effects made in BFXR. (bfxr.net)\nMusic by Joth. (https://opengameart.org/users/joth)";
-disp.color = FlxColor.BLACK;
-disp.size = 15;
-disp.screenCenter();
-disp.y -= 100;
-add(disp);
-        super.create();
+      disp.text = "Enemy sprite by me. \nAll other art by Kenney. (Kenney.nl)\nSound Effects made in BFXR. (bfxr.net)\nMusic by Joth. (https://opengameart.org/users/joth)";
+      disp.color = FlxColor.BLACK;
+      disp.size = 15;
+      disp.screenCenter();
+      disp.y -= 100;
+      add(disp);
+      super.create();
     }
 
     override public function update(elapsed:Float):Void{
